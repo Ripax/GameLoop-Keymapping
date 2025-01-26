67886d379d9048be3c8a6e58d3edd1eed0faf0d1
Ripan007 <ripanbiswas007@gmail.com>
2025-01-27 00:30:03 +0530

    gameloop
    keyfix

---
+++ Kill_Gameloop.bat
@@ -0,0 +1,19 @@
+@echo off
+color 0b
+net stop aow_drv
+net stop Tensafe
+taskkill /f /im appmarket.exe
+taskkill /f /im androidemulator.exe
+taskkill /f /im aow_exe.exe
+taskkill /f /im QMEmulatorService.exe
+taskkill /f /im RuntimeBroker.exe
+taskkill /f /im adb.exe
+taskkill /f /im GameLoader.exe
+taskkill /f /im TSettingCenter.exe
+taskkill /f /im syzs_dl_svr.exe
+taskkill /f /im AndroidEmulator.exe
+taskkill /f /im AndroidEmulatorEn.exe
+taskkill /f /im AndroidEmulatorEx.exe
+net stop aow_drv
+net stop Tensafe
+
